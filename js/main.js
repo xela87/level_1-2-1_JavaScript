58d8df3ecbff9deaa951d2d04c1de4ea314bce86
Oleksii Khokhlov <xoxlov87@gmail.com>
2021-05-07 02:14:04 +0300

    Create rectangle task

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 let btn1 = document.getElementById('show')
 let btn2 = document.getElementById('delete')
 let btn3 = document.getElementById('hide')
+let counter = 0;
 
-btn1.onclick = () => document.getElementById('rectangle').style.display = 'none';
-btn2.onclick = () => document.getElementById('rectangle').remove();
-btn3.onclick = () => document.getElementById('rectangle').classList.add('hidden');
-document.getElementsByClassName('hidden')[0].classList.remove('hidden')
+btn1.onclick = () => {
+    if (counter % 2 !== 0 || counter === 0) {
+        document.getElementById('rectangle').style.display = 'none';
+    } else {
+        document.getElementById('rectangle').style.display = 'block';
+    }
+    counter++;
+}
+
+btn2.onclick = () => {
+    document.getElementById('rectangle').remove();
+}
+
+btn3.onclick = () => {
+    counter++;
+    if (counter % 2 !== 0 || counter === 0) {
+        document.getElementById('rectangle').classList.add('hidden');
+    } else {
+        document.getElementsByClassName('hidden')[0].classList.remove('hidden')
+    }
+}
