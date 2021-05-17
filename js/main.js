1a1c3ae57cf6b0147db2adcb11ed4bbf279f901e
Oleksii Khokhlov <xoxlov87@gmail.com>
2021-05-18 02:37:04 +0300

    added 14 subtasks, rework visibility parameters, added comments

--- js/main.js
+++ js/main.js
@@ -1,15 +1,8 @@
-const btn1 = document.getElementById('show')
-const btn2 = document.getElementById('delete')
-const btn3 = document.getElementById('hide')
-const btn4 = document.getElementById('hide-all')
-const btn5 = document.getElementById('hide-selectors');
-let counter = 0;
+/**Task 1, 2*/
+const btn1 = document.getElementById('show');
+const btn2 = document.getElementById('delete');
+const btn3 = document.getElementById('hide');
 const item = document.getElementById('rectangle');
-const yellow = document.getElementById('yellow-rectangle');
-const hoverBtn = document.getElementById('hover-button');
-const btn6 = document.getElementById('submit-link');
-const btn7 = document.getElementById('submit-links');
-
 btn1.onclick = () => {
     item.style.display = item.style.display === 'none' ? 'block' : 'none';
 }
@@ -27,6 +20,9 @@ btn3.onclick = () => {
     }
 }
 
+/**Task 3*/
+const btn4 = document.getElementById('hide-all');
+let counter = 0;
 btn4.onclick = () => {
     counter++;
     if (counter % 2 !== 0) {
@@ -40,25 +36,31 @@ btn4.onclick = () => {
     }
 }
 
+/**Task 4*/
+const btn5 = document.getElementById('hide-selectors');
 btn5.onclick = () => {
     let selector = document.getElementById('text').value;
     let selectors = document.querySelectorAll(selector);
     for (let i = 0; i < selectors.length; i++) {
-        selectors[i].style.opacity = selectors[i].style.opacity === "0" ? "1" : "0";
+        selectors[i].style.opacity = selectors[i].style.opacity === '0' ? '1' : '0';
     }
 }
 
+/**Task 5*/
+const yellow = document.getElementById('yellow-rectangle');
 yellow.onclick = () => {
     if (counter % 2 === 0) {
         ++counter;
         alert("Hello");
-        yellow.style.opacity = "1";
+        yellow.style.opacity = '1';
     } else {
         ++counter;
-        yellow.style.opacity = "0";
+        yellow.style.opacity = '0';
     }
 }
 
+/**Task 6*/
+const hoverBtn = document.getElementById('hover-button');
 let redRect = document.getElementById('red-rectangle');
 hoverBtn.onmouseover = () => {
     redRect.style.opacity = '1';
@@ -67,23 +69,25 @@ hoverBtn.onmouseout = () => {
     redRect.style.opacity = '0';
 }
 
+/**Task 7*/
 let inputText = document.getElementById('some-text');
 let greenRect = document.getElementById('green-rectangle');
-
 inputText.onfocus = () => {
     greenRect.style.opacity = '1';
 }
-
 inputText.oninput = () => {
     greenRect.style.opacity = '0';
 }
 
+/**Task 8*/
+const btn6 = document.getElementById('submit-link');
 let image = document.getElementById('image');
-
 btn6.onclick = () => {
     image.src = document.getElementById('image-link').value;
 }
 
+/**Task 9*/
+const btn7 = document.getElementById('submit-links');
 btn7.onclick = () => {
     const images = document.getElementById('links-area').value.split('\n');
     for (let link of images) {
@@ -93,53 +97,55 @@ btn7.onclick = () => {
     }
 }
 
-
+/**Task 10, 11, 12*/
 let cursor = document.getElementById('cursor-coordinates');
 let lang = document.getElementById('language');
 let geo = document.getElementById('geolocation');
-
-let localStorageText = document.getElementById('text1');
-// let cookieText = document.getElementById('text1');
-let sessionStorageText = document.getElementById('text3');
-
-localStorageText.oninput = () => {
-    localStorage.setItem('local', localStorageText.innerText);
-}
-
-if (localStorage.getItem('local') !== '') {
-    localStorageText.innerText = localStorage.getItem('local');
-}
-
-//
-// cookieText.oninput = () =>  {
-//
-// }
-
-sessionStorageText.oninput = () => {
-    sessionStorage.setItem('session', sessionStorageText.innerText);
-}
-
-if(sessionStorage.getItem('session') !== '') {
-    sessionStorageText.innerText = sessionStorage.getItem('session');
-}
 
 window.onload = () => {
     document.onmousemove = (event) => {
         cursor.innerHTML = 'Cursor coordinates: X: ' + event.pageX + ', Y: ' + event.pageY;
     }
-
     lang.innerHTML = 'Language: ' + navigator.language;
     navigator.geolocation.getCurrentPosition((position) => {
         geo.innerHTML = 'Your location: Ш: ' + position.coords.latitude + ', Д: ' + position.coords.longitude;
     });
 }
 
+/**Task 13*/
+let localStorageText = document.getElementById('text1');
+let cookieText = document.getElementById('text2');
+let sessionStorageText = document.getElementById('text3');
 
+localStorageText.oninput = () => {
+    localStorage.setItem('local', localStorageText.innerText);
+}
+if (localStorage.getItem('local') !== '') {
+    localStorageText.innerText = localStorage.getItem('local');
+}
 
+cookieText.oninput = () => {
+    document.cookie = cookieText.innerText;
+}
+cookieText.innerText = document.cookie;
 
+sessionStorageText.oninput = () => {
+    sessionStorage.setItem('session', sessionStorageText.innerText);
+}
+if (sessionStorage.getItem('session') !== '') {
+    sessionStorageText.innerText = sessionStorage.getItem('session');
+}
 
+/**Task 14*/
+const btn8 = document.getElementById('back-to-top');
+window.onscroll = () => {
+    if (scrollY + innerHeight >= document.body.scrollHeight) {
+        btn8.style.opacity = '1';
+    } else {
+        btn8.style.opacity = '0';
+    }
+}
+btn8.onclick = () => {
+    window.scrollTo({top: 0, behavior: "smooth"})
 
-
-
-
-
+}
